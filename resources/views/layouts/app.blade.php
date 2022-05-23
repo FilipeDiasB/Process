1f0868c27120953ff69c8b064df80d052dd8df9b
FilipeDiasB <filipe.dbitt@gmail.com>
2022-05-23 17:02:56 -0300

    fix header and framework

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,20 +7,19 @@
     <title>{{ config('app.name', 'Process') }}</title>
     <!-- Fonts -->
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+{{--    <link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="{{ asset('js/flowbite.js') }}"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
-<body class="bg-gray-100 font-sans leading-normal tracking-normal">
+<body class="font-sans leading-normal tracking-normal">
 
 <div class="flex md:flex-row-reverse flex-wrap">
 
     <!--Main Content-->
-<div class="w-full md:w-full bg-gray-100">
-    <div class="container bg-gray-100 pt-8 px-6 md:pl-52">
+<div class="w-full md:w-full">
+    <div class="container pt-8 px-6 md:pl-52">
         {{ $slot }}
     </div>
 </div>
