d9344493564b248995e80cc0ec099a79b12f528a
FilipeDiasB <filipe.dbitt@gmail.com>
2022-05-25 09:20:32 -0300

    teste

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,10 @@
     <title>{{ config('app.name', 'Process') }}</title>
     <!-- Fonts -->
     <!-- Styles -->
-{{--    <link rel="stylesheet" href="{{ asset('css/app.css') }}">--}}
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script src="https://cdn.tailwindcss.com"></script>
+{{--    <script src="https://cdn.tailwindcss.com"></script>--}}
 </head>
 
 <body class="font-sans leading-normal tracking-normal">
